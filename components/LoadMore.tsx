d4f09447f24890791d6dfccdba30ef580b5de416
ingaSiu <ingasiud@gmail.com>
2024-04-10 12:26:39 +0300

    remove unnecessary code

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -9,9 +9,11 @@ import { useInView } from 'react-intersection-observer';
 
 let page = 2;
 
+export type AnimeCard = JSX.Element;
+
 function LoadMore() {
   const { ref, inView } = useInView();
-  const [data, setData] = useState<AnimeProp[]>([]);
+  const [data, setData] = useState<AnimeCard[]>([]);
 
   useEffect(() => {
     if (inView) {
@@ -24,11 +26,7 @@ function LoadMore() {
 
   return (
     <>
-      <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
-        {data.map((item: AnimeProp, index: number) => (
-          <AnimeCard key={item.id} anime={item} index={index} />
-        ))}
-      </section>
+      <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">{data}</section>
       <section className="flex justify-center items-center w-full">
         <div ref={ref}>
           <Image src="./spinner.svg" alt="spinner" width={56} height={56} className="object-contain" />
