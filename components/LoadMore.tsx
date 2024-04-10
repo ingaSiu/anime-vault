79509d52c57ac65577c730454a3f98e5280f4513
ingaSiu <ingasiud@gmail.com>
2024-04-10 10:48:02 +0300

    add intersection observer package

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,17 +1,14 @@
-import Image from "next/image";
+import Image from 'next/image';
+import { useInView } from 'react-intersection-observer';
 
 function LoadMore() {
+  const { ref, inView } = useInView();
+
   return (
     <>
       <section className="flex justify-center items-center w-full">
         <div>
-          <Image
-            src="./spinner.svg"
-            alt="spinner"
-            width={56}
-            height={56}
-            className="object-contain"
-          />
+          <Image src="./spinner.svg" alt="spinner" width={56} height={56} className="object-contain" />
         </div>
       </section>
     </>
@@ -19,3 +16,4 @@ function LoadMore() {
 }
 
 export default LoadMore;
+
